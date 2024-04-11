256f26f5ce272a8dd39f04ae3422646898fc2ee3
root <root@ip-172-31-42-137.ec2.internal>
2024-04-11 04:27:51 +0000

    Added templates

--- {{cookiecutter.project_name}}/app.py
+++ {{cookiecutter.project_name}}/app.py
@@ -1,12 +1,11 @@
+# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# {{ cookiecutter.route_name }} will be replaced by the user-provided route name
-@app.route('{{ cookiecutter.route_url }}')
+@app.route("{{ cookiecutter.route_url }}")
 def {{ cookiecutter.route_function }}():
-    return render_template('{{ cookiecutter.template_name }}')
+    return render_template('{{ cookiecutter.template_name }}', title='{{ cookiecutter.title }}', greeting='{{ cookiecutter.greeting }}')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
